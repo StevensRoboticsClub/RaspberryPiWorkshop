14f94601195c5c62d30618a43b98f79951f89886
Kevin Ward <43688127+RaZeragon@users.noreply.github.com>
2022-04-18 21:10:07 -0400

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,14 +1,7 @@
-
-import RPi.GPIO as GPIO
 from gpiozero import Servo
 from time import sleep
 
-GPIO.setmode(GPIO.BOARD)
-
-PIN_SERVO = 7
-GPIO.setup(PIN_SERVO, GPIO.OUT)
-
-servo = Servo(PIN_SERVO)
+servo = Servo(7)
 position = -1 
 
 # will move the servo from postion -1
@@ -16,5 +9,4 @@ position = -1
 while position < 1:
     servo.value = position
     sleep(0.1)
-    position += 1 
-    
+    position += 1
